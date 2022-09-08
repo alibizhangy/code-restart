29a7041d613a90a82815c64eee744b05e271e364
alibizhangy <alibizhangy@gmail.com>
2022-09-08 06:01:17 -0700

    assigmant operator tested

---
+++ assigmant-operator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std ; 
+
+
+int main () 
+{
+    int x = 0 ; 
+
+    x = x + 10 ; 
+    cout << x << endl; 
+
+    x += 10 ; 
+    cout << x << endl; 
+
+    x++; 
+    cout << x << endl; 
+    
+    x += 10 ; 
+    cout << x << endl; 
+
+    x -= 10 ; 
+    cout << x << endl; 
+
+    x *= 10 ; 
+    cout << x << endl ;
+    x = x * 10 ;
+    cout << x << endl ;
+
+    x /= 10 ; 
+    cout << x << endl; 
+
+    x %= 10 ; 
+    cout << x << endl ;
+
+
+    
+    
+    return 0 ; 
+} 
