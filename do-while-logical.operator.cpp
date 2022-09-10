dbfaacd168c4fd022ca4974db0a00130c979c946
alibizhangy <alibizhangy@gmail.com>
2022-09-10 05:34:56 -0700

    create the file and doing a programing

--- do-while-logical.operator.cpp
+++ do-while-logical.operator.cpp
@@ -3,7 +3,20 @@ using namespace std ;
 
 int main () 
 {
-    
-    
+//     do 
+//     {
+
+//     }while(condition) ;  
+//     // logicall operator 
+//     i() and || and && 
+    int password ; 
+    do 
+    {
+        
+        cout << "enter your password ! " << endl; 
+        cin >> password ; 
+
+    }while(password != 123);
     
     return 0 ; 
+}
